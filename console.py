666bc58370f73b2e44c91fcd1b615b2bdb383410
aytachuseynli <aytac_ha@mail.ru>
2024-02-27 13:49:39 +0400

    console.py - the entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+    The Console
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb) "
+
+    def do_EOF(self, argv):
+        "EOF command to exit the program"
+        return True
+
+    def do_quit(self, argv):
+        "Quit command to exit the program"
+        return True
+
+    def do_salamarzu(self, argv):
+        "SalamArzu command to say hello Arzu"
+        print("Salam Arzu")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
