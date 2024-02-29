96145ec1b98505c213fc4cdd473322a16f9f1a82
aytachuseynli <aytac_ha@mail.ru>
2024-02-29 10:47:15 +0400

    Test of new method of FileStorage.

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 class TestFileStorage(TestCase):
     def setUp(self):
         self.fs = FileStorage()
+        self.b1 = BaseModel()
 
     def test_class_attributes(self):
         self.assertIsInstance(self.fs._FileStorage__file_path, str)
         self.assertIsInstance(self.fs._FileStorage__objects, dict)
+
+    def test_new(self):
+        self.assertIn(self.b1, self.fs.all().values())
