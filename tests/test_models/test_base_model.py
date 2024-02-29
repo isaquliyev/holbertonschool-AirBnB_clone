2ef7bf49b141049eed691ee9f67fbc0b964375bb
aytachuseynli <aytac_ha@mail.ru>
2024-02-29 17:37:13 +0400

    Test of save method updated.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,8 @@
+import os
 from unittest import TestCase
 from models.base_model import BaseModel
 from datetime import datetime
+from models.engine.file_storage import FileStorage
 
 class TestBaseModel(TestCase):
     def setUp(self):
@@ -9,6 +11,9 @@ class TestBaseModel(TestCase):
         self.updated_at = self.b1.updated_at.isoformat()
 
         self.b2 = BaseModel()
+        self.file_path = "file.json"
+
+        self.fs = FileStorage()
 
     def test_attributes(self):
         self.assertNotEqual(self.b1.id, self.b2.id)
@@ -24,9 +29,22 @@ class TestBaseModel(TestCase):
         self.b1.save()
         self.assertNotEqual(updated_at, self.b1.updated_at)
 
+        self.b1.name = "Arzu"
+        self.b1.my_number = 808
+        self.b1.save()
+        self.assertTrue(os.path.exists(self.file_path))
+        self.assertIn("BaseModel." + self.b1.id, self.fs.all())
+
     def test_to_dict(self):
         new_dict = self.b1.to_dict()
 
         self.assertEqual(new_dict['__class__'], BaseModel.__name__)
         self.assertEqual(new_dict['created_at'], self.created_at)
         self.assertEqual(new_dict['updated_at'], self.updated_at)
+
+    def tearDown(self):
+        try:
+            os.remove(self.file_path)
+        except IOError:
+            pass
+
